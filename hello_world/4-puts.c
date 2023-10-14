affe921cbdc05756c46255711539318b5721ae33
B2L91 <7505@holbertonstudents.com>
2023-10-14 13:43:34 -0700

    program C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,26 +1,18 @@
-/*
- *  * File: example.c
- *   * Author: [Votre nom]
- *    * Description: [Description du contenu du fichier]
- *     */
-
 #include <stdio.h>
 
-/**
- *  * main - La fonction principale
- *   *
- *    * Description:
- *     * Cette fonction est le point d'entrée du programme.
- *      *
- *       * Return:
- *        * Toujours 0
- *         */
-int main(void)
-{
-	    /*
-	     *      * La fonction puts est utilisée pour afficher une chaîne de caractères.
-	     *           */
-	    puts("Programming is like building a multilingual puzzle");
-	        return (0);
-}
 
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+	
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}	
