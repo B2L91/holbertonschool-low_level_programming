658f490ca61fb40d873ff21fd79ab6626b4f4333
B2L91 <7505@holbertonstudents.com>
2023-10-15 12:36:51 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- *  
- *  main - Script used to print a message without use printf*
- *  Return: 0 = success
- *  */
+ *  * main - prints "Programming is like building a multilingual puzzle
+ *   * puzzle, followed by a newline.
+ *    * Return: Always 0.
+*/
+
+
 int main(void)
+
+
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return (0);
+	        puts("\"Programming is like building a multilingual puzzle");
+	        return (0);
+
 }
