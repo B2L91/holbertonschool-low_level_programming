7d3814fbf3e461298354d13a44522b8533bcb17e
B2L91 <7505@holbertonstudents.com>
2023-10-15 13:29:59 -0700

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints "with proper grammar, but the outcome is
- * a piece of art," followed newline
- * Return: Always 0
- *
+ * main - function to print a message
+ * Return: =0  success
  */
-
 int main(void)
-
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}	
+}
