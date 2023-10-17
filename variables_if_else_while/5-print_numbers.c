bd65ab8b689fd8b8c625ccb514bbc184e1ee9a9c
B2L91 <7505@holbertonstudents.com>
2023-10-17 03:32:14 -0700

    print digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char digits;
+
+for (digits = 0; digits <= 10; digits++)
+{
+putchar(digits + '0');
+}
+
+putchar('\n');
+return (0);
+}
