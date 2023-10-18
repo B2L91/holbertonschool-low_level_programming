089817ac34484481aff2d05edee45a3204555025
B2L91 <7505@holbertonstudents.com>
2023-10-18 06:36:57 -0700

    O C functions

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'print putchar'
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+	return (0);
+}
+
+
