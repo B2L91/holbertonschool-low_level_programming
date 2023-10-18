32750b2229ec04c7c6d43b2e7001f028b922234a
B2L91 <7505@holbertonstudents.com>
2023-10-18 07:56:55 -0700

    1 alphabet lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print alphabet lowercase'
+ *
+ * Return: Always 0 (Sucess)
+ */
+void print_alphabet(void);
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
