c5cea87499714268323666e7c61583392173c0fd
B2L91 <7505@holbertonstudents.com>
2023-10-19 07:32:10 -0700

    checks lower case c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * islower - checks for lowercase character
+ * 
+ * Return: void
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 #include <unistd.h>
 
 int _putchar(char c);
+int _islower(int c);
 void print_alphabet_x10(void);
 #endif
