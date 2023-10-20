20ee95d8e9b2d358541b7120ae21cdcd6d1a4c42
B2L91 <7505@holbertonstudents.com>
2023-10-20 01:36:37 -0700

    4 letter lowercase or upercase

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,12 +4,12 @@
 /**
  * _isalpha - checks for letter, lowercase or uppercase
  * @c: check if character c is letter or uppercase
- *    
+ *
  *  Return: 1 for letter, lowercase or uppercase or 0 for othersize
  */
 int _isalpha(int c)
 
-{	
+{
 
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
