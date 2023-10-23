5fe5b9e8d01cd9c9a39634347273e69a36756f54
B2L91 <7505@holbertonstudents.com>
2023-10-23 08:36:13 -0700

    1 swap values

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * swap - swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: variable
  * @b: variable
  * Description: swap functions
