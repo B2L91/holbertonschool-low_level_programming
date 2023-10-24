8659d785f105ab1c7377c581de01012819bc3f88
B2L91 <7505@holbertonstudents.com>
2023-10-24 01:04:06 -0700

    2 lentgh of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: read a string
+ * Description: returns length a string
+ * Return: value
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+	length++;
+s++;
+}
+return (length);
+}
