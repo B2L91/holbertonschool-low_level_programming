43d6185e2d781cbccb7088316f201cfbcfeee0eb
B2L91 <7505@holbertonstudents.com>
2023-10-26 12:40:19 -0700

    6 puts print every caracter of string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * puts2 - prints every other character of a string
+ * @str: sequence of characters terminated with a null character '\0'
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i += 2;
+}
+}
+
