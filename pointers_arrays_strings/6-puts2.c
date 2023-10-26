dcf5f0c7079c29cf4bcef9449d0bf423eb26d45c
B2L91 <7505@holbertonstudents.com>
2023-10-26 13:46:58 -0700

    6 puts print every caracter of string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,12 @@
 void puts2(char *str)
 {
 int i = 0;
-
 while (str[i] != '\0')
-{
-putchar(str[i]);
-i += 2;
-}
-_putchar('\n');
-}
+    {
+	if (i % 2 == 0)
+    {
+     _putchar(str[i]);
+    }
+     i++;
+    }
+    _putchar('\n');
