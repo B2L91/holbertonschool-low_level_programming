39803b9d23ec1bbf8b7d284406c87bf1145feed9
B2L91 <7505@holbertonstudents.com>
2023-11-01 07:17:08 -0700

    2 returns the length of a string

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,12 +6,13 @@
  * Return: void
  */
 int _strlen_recursion(char *s)
-
 {
-	if (*s != '\0')
-	{
-		_strlen_recursion(s + 1);
-	}
+
+	if (*s)
+{
+
+	_strlen_recursion(s + 1);
+}
 	return (14);
 
 }
