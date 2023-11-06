c49738aaf4cb6512b79142a7fa8416e37580810e
B2L91 <7505@holbertonstudents.com>
2023-11-06 06:45:02 -0800

    2 strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,22 +5,22 @@
 * _strchr - locates a character in a string.
 * @s: string
 * @c: character
-* Return: pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+* Return: word
 */
 
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0') 
-    {
-        if (*s == c)
-       	{
-            return s; 
-        }
-        s++; 
-    }
-    if (c == '\0') 
-    {
-        return s; 
-    }
-    return NULL; 
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (c == '\0')
+{
+return (s);
+}
+return (NULL);
 }
