02162426f06d5bc21c4cb28703cfa08eaff2911f
B2L91 <7505@holbertonstudents.com>
2024-02-20 03:15:48 -0800

    prints exactly with proper grammar, but the outcome is a piece of art

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Return: Always 0.
  */
 
-int main() {
-    printf("%s, but the outcome is a piece of art,\n", "with proper grammar");
-    return 0;
+int main(void)
+printf("%s, but the outcome is a piece of art,\n", "with proper grammar");
+return (0);
 }
