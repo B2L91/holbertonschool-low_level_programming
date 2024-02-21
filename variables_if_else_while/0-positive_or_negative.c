6ce451d9a8e62e0598153236e8515e8cacf9293d
B2L91 <7505@holbertonstudents.com>
2024-02-21 09:14:24 -0800

    program will assign a random number to the variable

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  */
 
 int main(void)
-{	
+{
 int n;
 
 
@@ -16,7 +16,7 @@ int n;
 		n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-	printf("%d is negative\n", n);	
+	printf("%d is negative\n", n);
 }
 else if (n == 0)
 	{
